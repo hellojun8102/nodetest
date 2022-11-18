d24a76952ba4925c2a6c7a77f31288649bf81a79
hellojun8102 <118529784+hellojun8102@users.noreply.github.com>
2022-11-18 19:55:58 +0900

    first commit
    처음 올리는 api 테스트

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+var cors = require('cors')
+
+ 
+app.use(cors())
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+app.get('/sound/:name', function (req, res) {
+   const {name}=req.params
+    if(name=='dog'){
+        res.json({'sound':'멍멍'})
+    }else if(name=='cat'){
+        res.json({'sound':'야옹'})
+    }
+
+
+  })
+app.listen(3000) 
